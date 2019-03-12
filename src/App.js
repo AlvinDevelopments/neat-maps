8a269f142f75ed466e493152e4f27798de24a1f4
= <alvinl2@me.com>
2019-03-12 13:35:17 +0800

    Create Main App component with Screen Routing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav/Nav';
+import { Button } from '@material-ui/core';
+import { Redirect, Switch, Route } from 'react-router-dom';
+import MainPage from './page/Main/MainPage';
+import LoginPage from './page/Login/LoginPage';
+import UploadMenu from './components/Upload/Menu';
+import { compose } from 'recompose';
+import { connect } from 'react-redux';
+
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Nav>
+        <Button>
+            Logout
+        </Button>
+      </Nav>
+      <UploadMenu open={this.props.uploadModal}/>
+      <Switch>
+        <Route exact path ='/' component={()=>(<Redirect to={{pathname:'/main'}}/>)}/>
+        <Route exact path='/main' component={MainPage}/>
+        <Route exact authed={false} path='/login' component={LoginPage}/>
+      </Switch>
       </div>
     );
   }
 }
 
-export default App;
+const PrivateRoute = ({ ...props}) => (
+  <Route
+  render={
+    props =>
+     props.authed ?
+      <MainPage {...props} />
+      :
+      <Redirect
+      to={{
+        pathname:'/login'
+      }} 
+      />
+  }
+  />
+)
+
+const mapStateToProps = (state) => ({
+  uploadModal: state.uploadModal.isOpen
+})
+
+export default compose(
+  connect(
+    mapStateToProps
+  )
+)(App);
