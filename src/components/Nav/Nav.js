46543f130415cfc097b618ae2581092539b028a3
= <alvinl2@me.com>
2019-03-13 14:06:54 -0600

    Fix Nav bar padding

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import { Typography, Divider } from '@material-ui/core';
+import { Typography, Divider, AppBar } from '@material-ui/core';
 
 const Nav = (props) => {
     return (
-        <div style={{padding:'0.5% 2%'}}>
-            <Grid container justify='space-between'>
+            <Grid style={{padding:'0.5% 2%'}} container justify='space-between'>
                 <Grid item>
-                <Typography style={{display:'inline-block'}} variant='h6'>
-                    neat
-                </Typography>
-                &nbsp;
-                <Typography style={{display:'inline-block'}} variant='h6'>
-                    maps
-                </Typography>
+                <a href='/'>
+                    <Typography style={{display:'inline-block'}} variant='h4'>
+                        neat maps
+                    </Typography>
+                </a>
                 </Grid>
                 {props.children}
             </Grid>
-        </div>
     )
 };
 
