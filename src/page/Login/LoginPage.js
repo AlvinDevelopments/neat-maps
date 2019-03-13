9051c8c90236651d3e541cd1f402e8035438de54
= <alvinl2@me.com>
2019-03-13 14:06:10 -0600

    Stylize Login Page

--- src/page/Login/LoginPage.js
+++ src/page/Login/LoginPage.js
@@ -1,16 +1,42 @@
 import React from 'react';
-import { Paper } from '@material-ui/core';
+import { Paper, Typography } from '@material-ui/core';
 import LoginMenu from '../../components/Auth/LoginMenu';
+import { withStyles } from '@material-ui/core/styles';
+
+
+const styles = (theme) => ({
+    authBox: {
+        // ...getCenter(),
+        // position:'absolute',
+        padding:'2% 5%',
+        width:'300px',
+        height: '200px',
+        margin: 'auto'
+    }
+});
+
+const getCenter = () => {
+    return {
+        left: `50%`,
+        top: `50%`
+    }
+}
 
 const LoginPage = (props) => {
+    const { classes } = props;
     return (
         <div>
-            <Paper>
-                Login
+            <Paper className={classes.authBox}>
+                <Typography variant='h4'>
+                    Login
+                </Typography>
                 <LoginMenu/>
             </Paper>
         </div>
     )
 }
 
-export default LoginPage;
+
+
+
+export default withStyles(styles)(LoginPage);
